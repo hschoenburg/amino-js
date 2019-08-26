2435f3173b29093569528339243c386e9a6f0dcf
hschoenburg <hschoenburg@gmail.com>
2019-08-26 16:43:16 -0700

    add greeter types. go mod tidy

--- go/extensions/types.go
+++ go/extensions/types.go
@@ -1,7 +1,29 @@
 package extensions
 
+import (
+	sdk "github.com/cosmos/amino-js/go/lib/cosmos/cosmos-sdk/types"
+)
+
 /*
 
 You can declare types here or import them directly into codec
+If you need SDK types, import the stripped-down version of the sdk's types
 
 */
+
+// MsgGreet defines the MsgGreet Message
+type MsgGreet struct {
+	Body      string
+	Sender    sdk.AccAddress
+	Recipient sdk.AccAddress
+}
+
+type Greeting struct {
+	Sender    sdk.AccAddress
+	Recipient sdk.AccAddress
+	Body      string
+}
+
+type QueryResGreetings map[string][]Greeting
+
+type GreetingsList []Greeting
