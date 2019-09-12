a75b707298e2c5c0aaf8146b452379c44252bc74
hschoenburg <hschoenburg@gmail.com>
2019-09-11 17:35:11 -0700

    add nameservice types

--- go/extensions/codec.go
+++ go/extensions/codec.go
@@ -9,4 +9,11 @@ func RegisterCodec(codec *amino.Codec) {
 	codec.RegisterConcrete(MsgGreet{}, "hellochain/MsgGreet", nil)
 	codec.RegisterConcrete(GreetingsList{}, "hellochain/GreetingsList", nil)
 	codec.RegisterConcrete(QueryResGreetings{}, "hellochain/QueryResGreetings", nil)
+	codec.RegisterConcrete(QueryResNames{}, "nameservice/QueryResNames", nil)
+	codec.RegisterConcrete(MsgDeleteName{}, "nameservice/MsgDeleteName", nil)
+	codec.RegisterConcrete(MsgBuyName{}, "nameservice/MsgBuyName", nil)
+	codec.RegisterConcrete(MsgSetName{}, "nameservice/MsgSetName", nil)
+	codec.RegisterConcrete(QueryResResolve{}, "nameservice/QueryResResolve", nil)
+	codec.RegisterConcrete(Whois{}, "nameservice/Whois", nil)
+
 }
